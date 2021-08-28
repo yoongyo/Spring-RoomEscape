bba8ce51a40b3f0e1387f44e671e837603dfd966
yoongyo <33406406+yoongyo@users.noreply.github.com>
2021-08-28 15:05:53 +0900

    first commit

---
+++ src/test/java/bizchoollab/roomEscape/RoomEscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package bizchoollab.roomEscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomEscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
